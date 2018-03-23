f140baafa629b1f25df0d1f41cda1f5de526129a
Preston Bayle <prestonbayle@gmail.com>
2018-03-22 19:31:12 -0600

    finished mini

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  domain: 'preston24.auth0.com',
+  clientID: 'A_MFuUeQjh0Rsw2XAp12JUsAbprPleVc',
+  clientSecret: 'ktKcAJT7KW684G2LOc8ZilcdyZIPUXGAnAD64IDd2QNGfSxgh0cEpTFSHLFj9UFW'
+}
---
+++ strategy.js
@@ -0,0 +1,11 @@
+const config = require('./config');
+const Auth0Strategy = require('passport-auth0');
+
+module.exports = new Auth0Strategy({
+  domain: config.domain,
+  clientID: config.clientID,
+  clientSecret: config.clientSecret,
+  callbackURL: '/login'
+}, function(accessToken, refreshToken, extraParams, profile, done) {
+  return done(null, profile);
+});
